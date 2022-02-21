f5ae07b0887923009c265a7483b65b5bc1fd7de0
syegireddi@turingsystems.com <syegireddi@turingsystems.com>
2022-02-21 15:13:11 +0530

    Add project files.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DemoEncap
+{
+    class DemoEncap    {
+        private String studentName;
+        private int studentAge;
+        public String Name
+        {
+            get
+            {
+                return studentName;
+            }
+            set
+            {
+                studentName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return studentAge;
+            }
+            set
+            {
+                studentAge = value;
+            }
+        }
+    }
+    class GFG
+    {
+        static public void Main()
+        {
+          DemoEncap obj = new DemoEncap();
+            obj.Name = "Ankita";
+            obj.Age = 21;
+            Console.WriteLine("Name:" + obj.Name);
+            Console.WriteLine("Age:" + obj.Age);
+
+
+
+        }
+    }
+}
